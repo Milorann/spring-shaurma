ebfd75ae4ce29f64fb978b962671000afdc2e5eb
Milorann <o.listerenko@gmail.com>
2023-06-17 11:31:26 +0300

    Added order page.

--- src/main/java/com/kpo/springshaurma/controller/OrderController.java
+++ src/main/java/com/kpo/springshaurma/controller/OrderController.java
@@ -1,11 +1,24 @@
 package com.kpo.springshaurma.controller;
 
+import com.kpo.springshaurma.service.ServiceSample;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+@Slf4j
 @Controller
+@RequestMapping("/orders/current")
 @SessionAttributes("order")
+@RequiredArgsConstructor
 public class OrderController {
+    private final ServiceSample serviceSample;
 
+    @GetMapping
+    public String home() {
+        return "order";
+    }
     // Don't forget to inject SessionStatus sessionStatus
 }
